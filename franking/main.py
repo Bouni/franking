e7bb0a560323b82579b8577e89c9cf4b29c6dc31
Bouni <bouni@owee.de>
2026-04-07 19:14:29 +0200

    Only show paid invoices

--- franking/main.py
+++ franking/main.py
@@ -67,7 +67,7 @@ def index(
         FROM invoice_items ii
         INNER JOIN invoices i ON ii.invoice_id = i.id
         INNER JOIN customers c ON i.customer_id = c.id
-        WHERE ii.description LIKE '%Versand%' AND (i.status = 'sent' OR i.status = 'paid')
+        WHERE ii.description LIKE '%Versand%' AND i.status = 'paid'
         ORDER BY i.created_at DESC
     """)
     orders = [dict(order) for order in cursor.fetchall()]
