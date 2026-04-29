a295037f517bf0d39606b85931e1c82f9203506a
Bouni <bouni@owee.de>
2026-04-29 12:44:42 +0200

    Add post icon

--- frontend/src/components/Internetmarke.vue
+++ frontend/src/components/Internetmarke.vue
@@ -3,11 +3,19 @@
     <v-col cols="12">
       <v-chip
         variant="flat"
-        color="indigo"
-        prepend-icon="mdi-cash"
+        color="#ffcc00"
         size="large"
       >
         {{ (internetmarke.balance / 100).toFixed(2) }} €
+        <template v-slot:prepend>
+                <v-img
+                  width="16"
+                  :src="
+                      postIcon
+                  "
+                  class="mr-2"
+                ></v-img>
+        </template>
       </v-chip>
     </v-col>
   </v-row>
@@ -17,6 +25,7 @@
 import { useAppStore } from "@/store/app";
 import { storeToRefs } from "pinia";
 import { onMounted } from "vue";
+import postIcon from "@/assets/Post.svg";
 
 const appStore = useAppStore();
 const { internetmarke } = storeToRefs(appStore);
